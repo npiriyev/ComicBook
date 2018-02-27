8e6aa13300d72eba8dc3f4e514282748ec4882ce
lighto <npiriyev@live.com>
2018-02-27 13:48:52 +0200

    1st

---
+++ src/ComicBook/Controllers/Home.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBook.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            var content = new ContentResult();
+           
+            return "Hi there";
+        }
+
+    }
+}
